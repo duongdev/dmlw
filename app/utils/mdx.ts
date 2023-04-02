dca6a6dcb1379bddfc6b622cd8891fed2eb90d2b
Dustin Do <dustin.do95@gmail.com>
2023-04-03 01:59:08 +0700

    Move /contents to /app

--- app/utils/mdx.ts
+++ app/utils/mdx.ts
@@ -8,7 +8,7 @@ import typescript from 'highlight.js/lib/languages/typescript'
 import { readFile } from './fs.server'
 import { bundleMDX } from './mdx.server'
 
-export const CONTENTS_DIR = path.join(process.cwd(), 'contents')
+export const CONTENTS_DIR = path.join(__dirname, '../contents')
 
 export type MDXContent = {
   slug: string
